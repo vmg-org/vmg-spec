2acb9cb1b6b6a7313c43554d6f05abb797ee02a0
IvanRave <reyvart@hotmail.com>
2014-10-02 14:35:29 +0400

    chore(tasks): fix tasks
    missing libs
    
    time: 0h:5

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jshintReporter = require('jshint-stylish');
 
+var gitLog = require('git-log');
+var exec = require('child_process').exec;
 var filePath = {
   jshintBase: {
     src: ['./*.js', './*.json']
@@ -12,4 +14,22 @@ gulp.task('default', function() {
   gulp.src(filePath.jshintBase.src)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter(jshintReporter));
+});
+
+gulp.task('gitlog', function(done) {
+  var tmpFilePath = 'doc/log-tmp.log';
+  var logFilePath = 'doc/log-201409.log';
+  var afterDate = new Date(2014, 8, 2); //new Date(Date.now() - (1000 * 60 * 60 * 24));
+  var beforeDate = new Date(2014, 9, 1);
+
+  var shellCommand = 'git log ' + gitLog.generateArgs(afterDate, beforeDate, tmpFilePath).join(' ');
+  console.log(shellCommand);
+
+  exec(shellCommand, function(err) {
+    if (err) {
+      return done(err);
+    }
+
+    gitLog.createLog(tmpFilePath, logFilePath, done);
+  });
 });
