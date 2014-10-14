619bc0373f03bc25c49be81d5b3e4af68892b9aa
IvanRave <reyvart@hotmail.com>
2014-10-15 03:04:13 +0400

    feat(jobSource): add definition
    a job to create source file
    
    time: 0h:15

---
+++ src/defs/job_source.js
@@ -0,0 +1,39 @@
+module.exports = {
+  "primaryKey": "id",
+  "properties": {
+    "id": {
+      "type": "string", // ID from Elastic Transcoder \d-\w
+      "maxLength": 50,
+      "description": "Id from Elastic Transcoder"
+    },
+    "id_of_media_spec": {
+      "type": "integer",
+      "description": "This job for this media spec",
+      zref: {
+        mdl: 'media_spec',
+        fld: 'id',
+        del: 'cascade'
+      }
+    },
+    id_of_container_format: {
+      "type": "string",
+      maxLength: 50,
+      description: 'mime type',
+      zref: {
+        mdl: 'container_format',
+        fld: 'id',
+        del: 'restrict'
+      }
+    },
+    url_to_upload: {
+      "type": "string",
+      "maxLength": 2000,
+      "description": "Url to upload the result file"
+    },
+    url_to_read: {
+      type: 'string',
+      maxLength: 2000,
+      description: 'Url to read the resulted file'
+    }
+  }
+};
