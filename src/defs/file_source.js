332c08e6011ba0ef016aef9e9602319a2595bdcf
IvanRave <reyvart@hotmail.com>
2014-10-15 08:10:18 +0400

    feat(fileSource): add definition
    and correct a media_file
    
    time: 0h:10

---
+++ src/defs/file_source.js
@@ -0,0 +1,23 @@
+module.exports = {
+  primaryKey: 'id_of_media_spec',
+  properties: {
+    "id_of_media_spec": {
+      "type": "integer",
+      "description": "Id of a media spec",
+      zref: {
+        mdl: 'media_spec',
+        fld: 'id',
+        del: 'cascade'
+      }
+    },
+    "id_of_media_file": {
+      "type": "integer",
+      "description": "Id of a media file",
+      "zref": {
+        "mdl": "media_file",
+        "fld": "id",
+        "del": "cascade"
+      }
+    }
+  }
+};
--- src/defs/media_file.js
+++ src/defs/media_file.js
@@ -5,15 +5,6 @@ module.exports = {
       "type": "integer",
       "description": "Id of a media file with specific multimedia container format"
     },
-    "id_of_media_spec": {
-      "type": "integer",
-      "description": "Id of a media specification",
-      "zref": {
-        "mdl": "media_spec",
-        "fld": "id",
-        "del": "cascade"
-      }
-    },
     "id_of_container_format": {
       "type": "string",
       "description": "Id of a container format",
@@ -29,11 +20,6 @@ module.exports = {
       "description": "Url to the file",
       "maxLength": 2000
     },
-    "url_to_upload": {
-      "type": "string",
-      "description": "Url to upload the file, active during 10 seconds",
-      "maxLength": 2000
-    },
     "size": {
       "type": "integer",
       "description": "Size in bytes",
@@ -43,30 +29,6 @@ module.exports = {
       "type": "integer",
       "format": "int64",
       "description": "Original file and cutted files have diff duration, in milliseconds"
-    },
-    "progress_creation": {
-      "type": "integer",
-      "description": "Uploading or conversion",
-      "format": "int16"
-    },
-    "progress_cutting": {
-      "type": "integer",
-      "description": "Progress of cutting, for original = 100",
-      "format": "int16"
-    },
-    "cutting_start": {
-      "type": "integer",
-      "description": "Cutting time, start position, in milliseconds, for original = 0",
-      "format": "int64"
-    },
-    "cutting_stop": {
-      "type": "integer",
-      "description": "Cutting time, stop position, in milliseconds, for original = duration, id duration 0 then 0 (nothing cut)",
-      "format": "int64"
-    },
-    "is_original": {
-      "type": "boolean",
-      "description": "Whether the media file is original"
     }
   }
 };
